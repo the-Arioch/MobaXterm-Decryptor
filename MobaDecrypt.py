3041cce7f0417612ed6837d2b19723c85b5675d8
Arioch 'The <AriochThe+GH@gmail.com>
2023-07-02 20:43:12 +0300

    ini parser and iterator, but does not actually decode

--- MobaDecrypt.py
+++ MobaDecrypt.py
@@ -2,7 +2,7 @@
 import sys, os, platform, random, base64, binascii
 from Crypto.Hash import SHA512
 from Crypto.Cipher import AES
-import sys
+import configparser, base64
 
 ########### Crypto Code borrowed from https://github.com/HyperSine/how-does-MobaXterm-encrypt-password
 class MobaXtermCrypto:
@@ -49,11 +49,11 @@ class MobaXtermCrypto:
         return bytes(key)
 
 ################### THIS DECODES HASHES THAT ARE FROM INI FILES
-    def DecryptCredential(self, Ciphertext: str) -> bytes:
+    def DecryptCredential(self, Ciphertext: bytes) -> bytes:
         key = self._KeyCrafter()
 
         ct = bytearray()
-        for char in Ciphertext.encode('ascii'):
+        for char in Ciphertext:
             if char in key:
                 ct.append(char)
 
@@ -71,11 +71,11 @@ class MobaXtermCrypto:
             raise ValueError('Invalid ciphertext.')
 
 #################### IF STORED IN REGISTRY YOU NEED 2 MORE PARAMS: THE CONNECTION'S IP/HOST and USERNAME
-    def DecryptPassword(self, Ciphertext: str, ConnHostname: bytes, ConnUsername: bytes) -> bytes:
+    def DecryptPassword(self, Ciphertext: bytes, ConnHostname: bytes, ConnUsername: bytes) -> bytes:
         key = self._KeyCrafter(ConnHostname = ConnHostname, ConnUsername = ConnUsername)
 
         ct = bytearray()
-        for char in Ciphertext.encode('ascii'):
+        for char in Ciphertext:
             if char in key:
                 ct.append(char)
 
@@ -95,9 +95,13 @@ class MobaXtermCrypto:
 
 def DecryptLine(SysHostname: str, SysUsername: str, SessionP: str, Ciphertext: str, 
                 ConnHostname: str = None, ConnUsername: str = None):
-    Host_B = SysHostname.encode('mbcs')
-    User_B = SysUsername.encode('mbcs')
-    Sess_B = SessionP.encode('mbcs')
+    # CharSet = 'cp1251'
+    CharSet = 'mbcs'
+    
+    Host_B = SysHostname.encode(CharSet)
+    User_B = SysUsername.encode(CharSet)
+    Sess_B = SessionP.encode(CharSet)
+    Cith_B = Ciphertext.encode(CharSet)  # encode('ascii')
     
     print("[*] Computer: "+SysHostname)
     print("[*] Username: "+SysUsername)
@@ -107,18 +111,41 @@ def DecryptLine(SysHostname: str, SysUsername: str, SessionP: str, Ciphertext: s
     cipher = MobaXtermCrypto(Host_B,User_B,Sess_B)
     
     if (ConnHostname is None) or (ConnUsername is None):
-      passw = cipher.DecryptCredential(Ciphertext)
+      passw = cipher.DecryptCredential(Cith_B)
     else:
-      ConnHost_B = ConnHostname.encode('mbcs')
-      ConnUser_B = ConnUsername.encode('mbcs')
+      ConnHost_B = ConnHostname.encode(CharSet)
+      ConnUser_B = ConnUsername.encode(CharSet)
       
       print("[*] Host/IP:  "+ConnHostname)
       print("[*] User:     "+ConnUsername)
       
-      passw = cipher.DecryptPassword(Ciphertext,ConnHost_B,ConnUser_B)
+      passw = cipher.DecryptPassword(Cith_B,ConnHost_B,ConnUser_B)
       
-    print("[*] Password: " + passw.decode("mbcs"))
+    print("[*] Password: " + passw.decode(CharSet))
     print()
+    
+def DecryptINI(FileName: str = None):
+    if FileName is None: FileName = "MobaXterm.ini"
+    FileName = os.path.abspath(FileName)
+
+    print("[*] ini-file: "+FileName)
+    print()
+    
+    config = configparser.RawConfigParser(delimiters=('='))
+    config.read(FileName) # locale.getencoding() ... on Windows, return the ANSI code page.
+    
+    iniSessionP = config['Misc']['SessionP']
+    iniUser = config['Misc']['MPSetAccount']
+    iniHost = config['Misc']['MPSetComputer']
+    
+    iniPwds = config['Passwords']
+    for key in iniPwds:
+      if key.lower() == 'mobauser@mobaserver': continue # can not be decripted this way
+      key2 = key.rsplit('@',1)
+      key3 = key2[0].rpartition(':')[-1]
+      # PayLoad = base64.standard_b64decode(iniPwds[key]).decode("mbcs")
+      DecryptLine(iniHost,iniUser,iniSessionP,iniPwds[key],key2[1],key3)
+    
 
 print('''
   __  __       _          __  ___
@@ -140,9 +167,17 @@ if len(sys.argv) == 5:
 if len(sys.argv) == 7:
     DecryptLine(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
     sys.exit(0) # correct exit
+if (len(sys.argv) == 2) and (sys.argv[1].lower() == 'ini'):
+    DecryptINI()
+    sys.exit(0) # correct exit
+if (len(sys.argv) == 3) and (sys.argv[1].lower() == 'ini'):
+    DecryptINI(sys.argv[2])
+    sys.exit(0) # correct exit
 
 print("Usage:\n")
 print("From inifile:")
+print("    MobaDecrypt.py ini Path\\To\\MobaXTerm.ini")
+print("    MobaDecrypt.py ini    // look for the file in the current path")
 print("    MobaDecrypt.py Computer Username SessionP Hash\n")
 print("From Registry Password:")
 print("    MobaDecrypt.py Computer Username SessionP Hash Host/IP User")
